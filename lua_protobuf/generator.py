63d66cdb486ef53ff0de01c67fd5756ccb70de32
Gregory Szorc <gregory.szorc@gmail.com>
2010-09-04 20:54:53 -0700

    numerous implementation improvements

--- lua_protobuf/generator.py
+++ lua_protobuf/generator.py
@@ -64,11 +64,27 @@ extern "C" {
 // if returns 0, Lua will not free the memory
 typedef int (*lua_protobuf_gc_callback)(::google::protobuf::Message *msg, void *userdata);
 
+// GC callback function that always returns true
+LUA_PROTOBUF_EXPORT int lua_protobuf_gc_always_free(::google::protobuf::Message *msg, void *userdata);
+
 #ifdef __cplusplus
 }
 #endif
 
 #endif
+
+'''
+
+def lua_protobuf_source():
+    '''Returns source for common code'''
+    return '''
+
+#include "lua-protobuf.h"
+
+int lua_protobuf_gc_always_free(::google::protobuf::Message *msg, void *ud)
+{
+    return 1;
+}
 
 '''
 
@@ -217,105 +233,231 @@ def field_get(package, message, field_descriptor):
 
     name = field_descriptor.name
     type = field_descriptor.type
+    label = field_descriptor.label
+    repeated = label == FieldDescriptor.LABEL_REPEATED
 
     lines = []
     lines.extend(field_function_start(package, message, 'get', name))
     lines.extend(obtain_message_from_udata(package, message))
 
-    # push a string or nil, depending if the field is set
-    if type == FieldDescriptor.TYPE_STRING or type == FieldDescriptor.TYPE_BYTES:
-        lines.append('string s = m->%s();' % name)
-        lines.append('m->has_%s() ? lua_pushlstring(L, s.c_str(), s.size()) : lua_pushnil(L);' % name)
+    # the logic is significantly different depending on if the field is
+    # singular or repeated.
+    # for repeated, we have an argument which points to the numeric index to
+    # retrieve. in true Lua convention, we index starting from 1, which is
+    # different from protocol buffers, which indexes from 0
 
-    elif type == FieldDescriptor.TYPE_BOOL:
-        lines.append('m->has_%s() ? lua_pushboolean(L, m->%s()) : lua_pushnil(L);' % ( name, name ))
+    if repeated:
+        lines.extend([
+            'if (lua_gettop(L) != 2) {',
+                'return luaL_error(L, "missing required numeric argument");',
+            '}',
+            'lua_Integer index = luaL_checkinteger(L, 2);',
+            'if (index < 1 || index > m->%s_size()) {' % name,
+                # TODO is returning nil the more Lua way?
+                'return luaL_error(L, "index must be between 1 and current size: %%d", m->%s_size());' % name,
+            '}',
+        ])
 
-    elif type in [FieldDescriptor.TYPE_INT32, FieldDescriptor.TYPE_UINT32,
-        FieldDescriptor.TYPE_FIXED32, FieldDescriptor.TYPE_SFIXED32, FieldDescriptor.TYPE_SINT32]:
-        lines.append('m->has_%s() ? lua_pushinteger(L, m->%s()) : lua_pushnil(L);' % ( name, name ))
+    # TODO float and double types are not equivalent. don't treat them as such
+    # TODO figure out how to support 64 bit integers properly
 
-    elif type == FieldDescriptor.TYPE_FLOAT or type == FieldDescriptor.TYPE_DOUBLE:
-        lines.append('m->has_%s() ? lua_pushnumber(L, m->%s()) : lua_pushnil(L);' % ( name, name ))
+    # TODO support the follwoing
+    #   FieldDescriptor.TYPE_GROUP
+    #   FieldDescriptor.TYPE_MESSAGE
+    #   FieldDescriptor.TYPE_ENUM
 
+    if repeated:
+        if type in [ FieldDescriptor.TYPE_STRING, FieldDescriptor.TYPE_BYTES ]:
+            lines.extend([
+                'string s = m->%s(index - 1);' % name,
+                'lua_pushlstring(L, s.c_str(), s.size());',
+            ])
+        elif type == FieldDescriptor.TYPE_BOOL:
+            lines.append('lua_pushboolean(L, m->%s(index-1));' % name)
+
+        elif type in [FieldDescriptor.TYPE_INT32, FieldDescriptor.TYPE_UINT32,
+            FieldDescriptor.TYPE_FIXED32, FieldDescriptor.TYPE_SFIXED32, FieldDescriptor.TYPE_SINT32]:
+
+            lines.append('lua_pushinteger(L, m->%s(index-1));' % name)
+
+        elif type in [ FieldDescriptor.TYPE_INT64, FieldDescriptor.TYPE_UINT64,
+            FieldDescriptor.TYPE_FIXED64, FieldDescriptor.TYPE_SFIXED64, FieldDescriptor.TYPE_SINT64]:
+            lines.append('lua_pushinteger(L, m->%s(index-1));' % name)
+
+        elif type == FieldDescriptor.TYPE_FLOAT or type == FieldDescriptor.TYPE_DOUBLE:
+            lines.append('lua_pushnumber(L, m->%s(index-1));' % name)
+
+        else:
+            lines.append('return luaL_errorL, "lua-protobuf does not support this field type");')
     else:
-        # not supported yet :(
-        lines.append('return luaL_error(L, "lua-protobuf does not support this field type");')
+        # for scalar fields, we push nil if the value is not defined
+        # this is the Lua way
+        if type == FieldDescriptor.TYPE_STRING or type == FieldDescriptor.TYPE_BYTES:
+            lines.append('string s = m->%s();' % name)
+            lines.append('m->has_%s() ? lua_pushlstring(L, s.c_str(), s.size()) : lua_pushnil(L);' % name)
 
-    #FieldDescriptor.TYPE_INT64: 'int64',
-    #FieldDescriptor.TYPE_UINT64: 'uint64',
-    #FieldDescriptor.TYPE_FIXED64: 'fixed64',
-    #FieldDescriptor.TYPE_GROUP: 'group',
-    #FieldDescriptor.TYPE_MESSAGE: 'message',
-    #FieldDescriptor.TYPE_ENUM: 'enum',
-    #FieldDescriptor.TYPE_SFIXED64: 'sfixed64',
-    #FieldDescriptor.TYPE_SINT64: 'sint64',
+        elif type == FieldDescriptor.TYPE_BOOL:
+            lines.append('m->has_%s() ? lua_pushboolean(L, m->%s()) : lua_pushnil(L);' % ( name, name ))
+
+        elif type in [FieldDescriptor.TYPE_INT32, FieldDescriptor.TYPE_UINT32,
+            FieldDescriptor.TYPE_FIXED32, FieldDescriptor.TYPE_SFIXED32, FieldDescriptor.TYPE_SINT32]:
+            lines.append('m->has_%s() ? lua_pushinteger(L, m->%s()) : lua_pushnil(L);' % ( name, name ))
+
+        elif type in [ FieldDescriptor.TYPE_INT64, FieldDescriptor.TYPE_UINT64,
+            FieldDescriptor.TYPE_FIXED64, FieldDescriptor.TYPE_SFIXED64, FieldDescriptor.TYPE_SINT64]:
+            lines.append('m->has_%s() ? lua_pushinteger(L, m->%s()) : lua_pushnil(L);' % ( name, name ))
+
+        elif type == FieldDescriptor.TYPE_FLOAT or type == FieldDescriptor.TYPE_DOUBLE:
+            lines.append('m->has_%s() ? lua_pushnumber(L, m->%s()) : lua_pushnil(L);' % ( name, name ))
+
+        else:
+            # not supported yet :(
+            lines.append('return luaL_error(L, "lua-protobuf does not support this field type");')
 
     lines.append('return 1;')
     lines.append('}\n')
 
     return lines
 
+def field_set_assignment(field, args):
+    return [
+        'if (index == current_size + 1) {',
+            'm->add_%s(%s);' % ( field, args ),
+        '}',
+        'else {',
+            'm->set_%s(index-1, %s);' % ( field, args ),
+        '}',
+    ]
+
 def field_set(package, message, field_descriptor):
     '''Returns function definition for a set_<field> function'''
 
     name = field_descriptor.name
     type = field_descriptor.type
+    label = field_descriptor.label
+    repeated = label == FieldDescriptor.LABEL_REPEATED
 
     lines = []
     lines.extend(field_function_start(package, message, 'set', name))
     lines.extend(obtain_message_from_udata(package, message, 1))
 
-    # if they call set() with nil, we interpret as a clear
-    # this is the Lua way, after all
-    lines.append('if (lua_isnil(L, 2)) {')
-    lines.append('m->clear_%s();' % name)
-    lines.append('lua_pop(L, 2);')
-    lines.append('return 0;')
-    lines.append('}\n')
-
-    if type == FieldDescriptor.TYPE_STRING or type == FieldDescriptor.TYPE_BYTES:
+    # we do things differently depending on if this is a singular or repeated field
+    # for singular fields, the new value is the first argument
+    # for repeated fields, the index is arg1 and the value is arg2
+    if repeated:
         lines.extend([
-            'if (!lua_isstring(L, 2)) return luaL_error(L, "passed value is not a string");',
-            'size_t len;',
-            'const char *s = lua_tolstring(L, 2, &len);',
-            'if (!s) {',
-            'luaL_error(L, "could not obtain string on stack. weird");',
+            'if (lua_gettop(L) != 3) {',
+            '    return luaL_error(L, "required 2 arguments not passed to function");',
             '}',
-            'm->set_%s(s, len);' % name,
-            'return 0;',
+            'lua_Integer index = luaL_checkinteger(L, 2);',
+            'size_t current_size = m->%s_size();' % name,
+            'if (index < 1 || index > current_size + 1) {',
+                'return luaL_error(L, "index must be between 1 and %d", current_size + 1);',
+            '}',
+
+            # we don't support the automagic nil clears value... yet
+            'if (lua_isnil(L, 3)) {',
+                'return luaL_error(L, "cannot assign nil to repeated fields (yet)");',
+            '}',
         ])
 
-    elif type in [ FieldDescriptor.TYPE_DOUBLE, FieldDescriptor.TYPE_FLOAT ]:
-        lines.extend([
-            'if (!lua_isnumber(L, 2)) return luaL_error(L, "passed value cannot be converted to a number");',
-            'lua_Number n = lua_tonumber(L, 2);',
-            'm->set_%s(n);' % name,
-            'return 0;',
-        ])
+    # TODO proper 64 bit handling
 
-    elif type == FieldDescriptor.TYPE_BOOL:
-        lines.extend([
-            'bool b = lua_toboolean(L, 2);',
-            'm->set_%s(b);' % name,
-            'return 0;',
-        ])
+    # TODO support following types
+    #    FieldDescriptor.TYPE_GROUP
+    #    FieldDescriptor.TYPE_MESSAGE
+    #    FieldDescriptor.TYPE_ENUM
 
+    # now move on to the assignment
+    if repeated:
+        if type in [ FieldDescriptor.TYPE_STRING, FieldDescriptor.TYPE_BYTES ]:
+            lines.extend([
+                'size_t length = 0;',
+                'const char *s = luaL_checklstring(L, 3, &length);',
+            ])
+            lines.extend(field_set_assignment(name, 's, length'))
+
+        elif type == FieldDescriptor.TYPE_BOOL:
+            lines.append('bool b = lua_toboolean(L, 3);')
+            lines.extend(field_set_assignment(name, 'b'))
+
+        elif type in [ FieldDescriptor.TYPE_DOUBLE, FieldDescriptor.TYPE_FLOAT ]:
+            lines.append('double d = lua_tonumber(L, 3);')
+            lines.extend(field_set_assignment(name, 'd'))
+
+        elif type in [ FieldDescriptor.TYPE_INT32, FieldDescriptor.TYPE_FIXED32,
+            FieldDescriptor.TYPE_UINT32, FieldDescriptor.TYPE_SFIXED32, FieldDescriptor.TYPE_SINT32 ]:
+
+            lines.append('lua_Integer i = lua_tointeger(L, 3);')
+            lines.extend(field_set_assignment(name, 'i'))
+
+        elif type in [ FieldDescriptor.TYPE_INT64, FieldDescriptor.TYPE_UINT64,
+            FieldDescriptor.TYPE_FIXED64, FieldDescriptor.TYPE_SFIXED64, FieldDescriptor.TYPE_SINT64]:
+
+            lines.append('lua_Integer i = lua_tointeger(L, 3);')
+            lines.extend(field_set_assignment(name, 'i'))
+        else:
+            lines.append('return luaL_error(L, "field type not yet supported");')
+
+        lines.append('return 0;')
     else:
-        lines.append('return luaL_error(L, "field type is not yet supported");')
+        # if they call set() with nil, we interpret as a clear
+        # this is the Lua way, after all
+        lines.extend([
+            'if (lua_isnil(L, 2)) {',
+                'm->clear_%s();' % name,
+                'return 0;',
+            '}',
+            '',
+        ])
 
-    #FieldDescriptor.TYPE_INT64: 'int64',
-    #FieldDescriptor.TYPE_UINT64: 'uint64',
-    #FieldDescriptor.TYPE_INT32: 'int32',
-    #FieldDescriptor.TYPE_FIXED64: 'fixed64',
-    #FieldDescriptor.TYPE_FIXED32: 'fixed32',
-    #FieldDescriptor.TYPE_GROUP: 'group',
-    #FieldDescriptor.TYPE_MESSAGE: 'message',
-    #FieldDescriptor.TYPE_UINT32: 'uint32',
-    #FieldDescriptor.TYPE_ENUM: 'enum',
-    #FieldDescriptor.TYPE_SFIXED32: 'sfixed32',
-    #FieldDescriptor.TYPE_SFIXED64: 'sfixed64',
-    #FieldDescriptor.TYPE_SINT32: 'sint32',
-    #FieldDescriptor.TYPE_SINT64: 'sint64',
+        if type in [ FieldDescriptor.TYPE_STRING, FieldDescriptor.TYPE_BYTES ]:
+            lines.extend([
+                'if (!lua_isstring(L, 2)) return luaL_error(L, "passed value is not a string");',
+                'size_t len;',
+                'const char *s = lua_tolstring(L, 2, &len);',
+                'if (!s) {',
+                    'luaL_error(L, "could not obtain string on stack. weird");',
+                '}',
+                'm->set_%s(s, len);' % name,
+                'return 0;',
+            ])
+
+        elif type in [ FieldDescriptor.TYPE_DOUBLE, FieldDescriptor.TYPE_FLOAT ]:
+            lines.extend([
+                'if (!lua_isnumber(L, 2)) return luaL_error(L, "passed value cannot be converted to a number");',
+                'lua_Number n = lua_tonumber(L, 2);',
+                'm->set_%s(n);' % name,
+                'return 0;',
+            ])
+
+        elif type in [ FieldDescriptor.TYPE_INT32, FieldDescriptor.TYPE_FIXED32,
+            FieldDescriptor.TYPE_UINT32, FieldDescriptor.TYPE_SFIXED32, FieldDescriptor.TYPE_SINT32 ]:
+
+            lines.extend([
+                'lua_Integer v = luaL_checkinteger(L, 2);',
+                'm->set_%s(v);' % name,
+                'return 0;',
+            ])
+
+        elif type in [ FieldDescriptor.TYPE_INT64, FieldDescriptor.TYPE_UINT64,
+            FieldDescriptor.TYPE_FIXED64, FieldDescriptor.TYPE_SFIXED64, FieldDescriptor.TYPE_SINT64]:
+
+            lines.extend([
+                'lua_Integer i = luaL_checkinteger(L, 2);',
+                'm->set_%s(i);' % name,
+                'return 0;',
+            ])
+
+        elif type == FieldDescriptor.TYPE_BOOL:
+            lines.extend([
+                'bool b = lua_toboolean(L, 2);',
+                'm->set_%s(b);' % name,
+                'return 0;',
+            ])
+
+        else:
+            lines.append('return luaL_error(L, "field type is not yet supported");')
 
     lines.append('}\n')
 
@@ -485,6 +627,7 @@ def message_method_array(package, descriptor):
     lines = []
     lines.append('static const struct luaL_Reg %s_methods [] = {' % message)
     lines.append('{"serialized", %sserialized},' % fp)
+    lines.append('{"clear", %sclear},' % fp)
     lines.append('{"__gc", %sgc},' % message_function_prefix(package, message))
 
     for fd in descriptor.field:
@@ -492,11 +635,11 @@ def message_method_array(package, descriptor):
         label = fd.label
 
         lines.append('{"clear_%s", %s},' % ( name, field_function_name(package, message, 'clear', name) ))
+        lines.append('{"get_%s", %s},' % ( name, field_function_name(package, message, 'get', name) ))
+        lines.append('{"set_%s", %s},' % ( name, field_function_name(package, message, 'set', name) ))
 
         if label in [ FieldDescriptor.LABEL_REQUIRED, FieldDescriptor.LABEL_OPTIONAL ]:
             lines.append('{"has_%s", %s},' % ( name, field_function_name(package, message, 'has', name) ))
-            lines.append('{"get_%s", %s},' % ( name, field_function_name(package, message, 'get', name) ))
-            lines.append('{"set_%s", %s},' % ( name, field_function_name(package, message, 'set', name) ))
 
         if label == FieldDescriptor.LABEL_REPEATED:
             lines.append('{"size_%s", %s},' % ( name, field_function_name(package, message, 'size', name) ))
@@ -557,19 +700,19 @@ def message_header(package, message_descriptor):
         '',
         '',
         '// constructor called from Lua',
-        'LUA_PROTOBUF_EXPORT int %s%s_new(lua_State *L);' % (function_prefix, message_name),
+        'LUA_PROTOBUF_EXPORT int %s%s_new(lua_State *L);' % ( function_prefix, message_name ),
         '',
         '// obtain instance from a serialized string',
-        'LUA_PROTOBUF_EXPORT int %s%s_parsefromstring(lua_State *L);' % (function_prefix, message_name),
+        'LUA_PROTOBUF_EXPORT int %s%s_parsefromstring(lua_State *L);' % ( function_prefix, message_name ),
         '',
         '// garbage collects message instance in Lua',
         'LUA_PROTOBUF_EXPORT int %s%s_gc(lua_State *L);' % ( function_prefix, message_name ),
         '',
         '// obtain serialized representation of instance',
-        'LUA_PROTOBUF_EXPORT int %s%s_serialized(lua_State *L);' % (function_prefix, message_name),
+        'LUA_PROTOBUF_EXPORT int %s%s_serialized(lua_State *L);' % ( function_prefix, message_name ),
         '',
         '// clear all fields in the message',
-        'LUA_PROTOBUF_EXPORT int %s%s_clear(lua_State *L);' % (function_prefix, message_name),
+        'LUA_PROTOBUF_EXPORT int %s%s_clear(lua_State *L);' % ( function_prefix, message_name ),
         '',
     ])
 
@@ -593,11 +736,11 @@ def message_header(package, message_descriptor):
 
         lines.append('// %s %s %s = %d' % (field_label_s, field_type_s, field_name, field_number))
         lines.append('LUA_PROTOBUF_EXPORT int %s%s_clear_%s(lua_State *L);' % (function_prefix, message_name, field_name))
+        lines.append('LUA_PROTOBUF_EXPORT int %s%s_get_%s(lua_State *L);' % (function_prefix, message_name, field_name))
+        lines.append('LUA_PROTOBUF_EXPORT int %s%s_set_%s(lua_State *L);' % (function_prefix, message_name, field_name))
 
         if field_label in [ FieldDescriptor.LABEL_REQUIRED, FieldDescriptor.LABEL_OPTIONAL ]:
             lines.append('LUA_PROTOBUF_EXPORT int %s%s_has_%s(lua_State *L);' % (function_prefix, message_name, field_name))
-            lines.append('LUA_PROTOBUF_EXPORT int %s%s_get_%s(lua_State *L);' % (function_prefix, message_name, field_name))
-            lines.append('LUA_PROTOBUF_EXPORT int %s%s_set_%s(lua_State *L);' % (function_prefix, message_name, field_name))
 
         if field_label == FieldDescriptor.LABEL_REPEATED:
             lines.append('LUA_PROTOBUF_EXPORT int %s%s_size_%s(lua_State *L);' % (function_prefix, message_name, field_name))
@@ -634,17 +777,14 @@ def message_source(package, message_descriptor):
         lines.extend(clear_body(package, message, name))
         lines.append('}\n')
 
+        lines.extend(field_get(package, message, descriptor))
+        lines.extend(field_set(package, message, descriptor))
+
         if descriptor.label in [FieldDescriptor.LABEL_OPTIONAL, FieldDescriptor.LABEL_REQUIRED]:
             # has_<field>()
             lines.extend(field_function_start(package, message, 'has', name))
             lines.extend(has_body(package, message, name))
             lines.append('}\n')
-
-            # get_<field>()
-            lines.extend(field_get(package, message, descriptor))
-
-            # set_<field>()
-            lines.extend(field_set(package, message, descriptor))
 
         if descriptor.label == FieldDescriptor.LABEL_REPEATED:
             # size_<field>()
